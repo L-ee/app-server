e24c1f7b91ef51ee36a5af9be5ed3a83efdc5bb1
Lee <646135832@qq.com>
2016-05-30 19:06:20 +0800

    修改baiduapi这个类

--- javautils/src/main/java/test/BaiduAPI.java
+++ javautils/src/main/java/test/BaiduAPI.java
@@ -1,5 +1,8 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.*;
@@ -9,59 +12,92 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by loovee1 on 2016/4/21.
+ * 根据经纬度值调用百度API获取定位点的地址信息
+ * @author Lee
+ * @Time 2016/4/21.
  */
 public class BaiduAPI {
 
     private static String ak = "5ef2641d89438a6e708db122820cf1d2";
 
-    public static Map<String, String> testPost(String x, String y) throws IOException {
-        URL url = new URL("http://api.map.baidu.com/geocoder?" + ak + "=您的密钥" +
-                "&callback=renderReverse&location=" + x
-                + "," + y + "&output=json");
-        URLConnection connection = url.openConnection();
-        /**
-         * 然后把连接设为输出模式。URLConnection通常作为输入来使用，比如下载一个Web页。
-         * 通过把URLConnection设为输出，你可以把数据向你个Web页传送。下面是如何做：
-         */
-        connection.setDoOutput(true);
-        OutputStreamWriter out = new OutputStreamWriter(connection
-                .getOutputStream(), "utf-8");
+    /*
+     * lat : 纬度。说明：与赤道平行的线就是纬度即X轴
+     * lng : 经度。说明：与赤道垂直的线就是经度即Y轴
+     */
+    public static Map<String, String> testPost(String lat, String lng) {
+
+        BufferedReader in = null;
+        InputStream l_urlStream = null;
+        OutputStreamWriter out = null;
+
+        StringBuilder urlAddress = new StringBuilder("http://api.map.baidu.com/geocoder?");
+        urlAddress.append(ak);
+        urlAddress.append("=您的密钥&callback=renderReverse&location=");
+        urlAddress.append(lat);
+        urlAddress.append(",");
+        urlAddress.append(lng);
+        urlAddress.append("&output=json");
+
+        try {
+            URL url = new URL(urlAddress.toString());
+            URLConnection connection = url.openConnection();
+            /**
+             * 然后把连接设为输出模式。URLConnection通常作为输入来使用，比如下载一个Web页。
+             * 通过把URLConnection设为输出，你可以把数据向你个Web页传送。下面是如何做：
+             */
+            connection.setDoOutput(true);
+            out = new OutputStreamWriter(connection.getOutputStream(), "utf-8");
 //        remember to clean up
-        out.flush();
-        out.close();
+            out.flush();
+            out.close();
 //        一旦发送成功，用以下方法就可以得到服务器的回应：
-        String res;
-        InputStream l_urlStream;
-        l_urlStream = connection.getInputStream();
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                l_urlStream,"UTF-8"));
-        StringBuilder sb = new StringBuilder("");
-        while ((res = in.readLine()) != null) {
-            sb.append(res.trim());
-        }
-        String str = sb.toString();
-        System.out.println(str);
-        Map<String,String> map = null;
-        if(StringUtils.isNotEmpty(str)) {
-            int addStart = str.indexOf("formatted_address\":");
-            int addEnd = str.indexOf("\",\"business");
-            if(addStart > 0 && addEnd > 0) {
-                String address = str.substring(addStart+20, addEnd);
-                map = new HashMap<String,String>();
-                map.put("address", address);
-                return map;
+            String res;
+
+            l_urlStream = connection.getInputStream();
+            in = new BufferedReader(new InputStreamReader(l_urlStream, "UTF-8"));
+            StringBuilder sb = new StringBuilder("");
+            while ((res = in.readLine()) != null) {
+                sb.append(res.trim());
+            }
+
+            if(StringUtils.isEmpty(sb.toString())){
+                return null;
+            }
+
+            return JSON.parseObject(sb.toString(),new TypeReference<Map<String,String>>(){});
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (l_urlStream != null){
+                try {
+                    l_urlStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(out != null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-        return null;
-
     }
 
     public static void main(String[] args) throws IOException {
         Map<String, String> json = BaiduAPI.testPost("23.881484","115.742757");
-        System.out.println("address :" + json.get("address"));
-        System.out.println("city:" + json.get("addressComponent"));
+        System.out.println("address :" + json.toString());
     }
 
 
