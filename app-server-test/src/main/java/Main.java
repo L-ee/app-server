c10a168b17aff91f69508514fb27445246798c5b
Lee <646135832@qq.com>
2017-07-22 17:17:26 +0800

    activemq

---
+++ app-server-test/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Function:
+ * Author:  ilene
+ * Date:    2017/5/18 12:53
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService
+
+
+    }
+}
